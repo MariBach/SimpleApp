df2bdb64cd5d164318260c61f5de49fee43b3850
Maryna Bachynska <bachinskaya.mari@gmail.com>
2021-11-02 11:14:22 +0200

    1.Replicate code from slide 11, create instances of car and truck. Try to call different methods for both, investigate output using console.log().

--- app.js
+++ app.js
@@ -1,14 +1,41 @@
-console.log("\nNice work! Your first application have been launched.");
-console.log("Now, I need to know your name, type it below: ");
-
-let stdin = process.openStdin();
-
-stdin.addListener("data", (txt) => {
-    if (txt.toString().trim() === "quit") {
-        console.log("\nHave a nice JS trip! Bye-bye");
-        process.exit(0);
-    } else {
-        console.log(`\nHi ${txt.toString().trim()}, nice to meet you at Eleks QA Academy 2021. I am sure you'll become great in your job.`);
-        console.log(`${txt.toString().trim()}, to quit this application you can use 'Ctrl + C' keys, or type 'quit' and hit 'Enter'`);
+class Vehicle {
+    constructor(carModel, carYear, maxSpeed) {
+        this.model = carModel;
+        this.year = carYear;
+        this.maxSpeed = maxSpeed;
     }
-});
+    displayInfo() {
+        console.log(`Name: ${this.model}; Year: ${this.year}; Max Speed: ${this.maxSpeed}`);
+    }
+}
+
+class Car extends Vehicle {
+    constructor(carModel, carYear, maxSpeed) {
+        super(carModel, carYear, maxSpeed);
+        this.type = 'car';
+    
+    }
+    transportPeople() {
+        console.log(`I am starting transporting passengers`);
+      
+    }
+}
+class Truck extends Vehicle {
+    constructor(carModel, carYear, maxSpeed) {
+        super(carModel, carYear, maxSpeed);
+        this.type = 'truck';
+    }
+    transportContainer() {
+        console.log(`I am starting transporting heavy container`);
+    }
+}
+let volvo = new Car("XC60", 2015, 220);
+let auto = new Vehicle("BMW", 2019, 210);
+let kamaz = new Truck("KAMAZ", 2010, 100);
+auto.displayInfo();
+volvo.displayInfo();
+volvo.transportPeople();
+kamaz.displayInfo();
+kamaz.transportContainer();
+
+
